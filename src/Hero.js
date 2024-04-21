d6d449c3b69bbf1377c7d29818aee127b24a1ac7
JuliaLotnikova <150443868+JuliaLotnikova@users.noreply.github.com>
2024-04-21 21:06:05 +0300

    v1

---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1 className="title">Supermarket</h1>
+      <p>Start shopping</p>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/Welcome.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Welcome = ({ name }) => {
+  return <h1>Привет, {name}!</h1>;
+};
+
+export default Welcome;
